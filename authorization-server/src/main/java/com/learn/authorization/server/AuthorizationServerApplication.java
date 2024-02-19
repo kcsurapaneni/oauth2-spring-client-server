8ff6fbf9537c7527470f3840da8aa1ccaa8a43be
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-02-18 23:45:41 -0700

    user details are added

--- authorization-server/src/main/java/com/learn/authorization/server/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/learn/authorization/server/AuthorizationServerApplication.java
@@ -1,13 +1,33 @@
 package com.learn.authorization.server;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.provisioning.*;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthorizationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var userBuilder = User.builder();
+        return new InMemoryUserDetailsManager(
+                userBuilder
+                        .roles("read")
+                        .username("user")
+                        .password("{bcrypt}$2a$12$7ZfJN0HyvbTC7ZAOXxDRvOQSzBMbPhZ0VIdXlzbMXOrOlq.LeS3zO")
+                        .build(),
+                userBuilder
+                        .roles("read", "write")
+                        .username("admin")
+                        .password("{bcrypt}$2a$04$78dIIJxxv/01FVBtXZOi1OKAUNeEcR7uxeoB/ZaZ1HY9up/hPGVLG")
+                        .build()
+        );
+    }
 
 }
